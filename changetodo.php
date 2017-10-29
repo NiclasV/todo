6f8a923ff168de8d536280cee57eccd81bd0730f
root <root@localhost.localdomain>
2017-10-29 13:34:19 +0100

    Add initial code to changetodo.php

--- changetodo.php
+++ changetodo.php
@@ -1,4 +1,26 @@
 <?php
+    require('db.php');
+    
+    $stmt = $pdo->query("SELECT id, title, completed, createdBy FROM todos ORDER BY id DESC"); //hämtar all data till $stmt
+    while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+        $id = $row['id'];
+        echo '<pre>';
+        echo "radiobox $id is examined";
+        echo '</pre>';
+        /*        array(3) {
+            ["radiobox-9"]=>
+            string(4) "done"
+            ["radiobox-8"]=>
+            string(6) "delete"
+            ["submit"]=>
+            string(6) "Change"
+          }
+  */
+        echo '<pre>';
+        if (isset($_POST["radiobox-" . $id]))
+          echo "radiobox $id is set to " .  $_POST["radiobox-" . $id];
+        echo '</pre>';
+    }
 
 echo '<pre>';
 var_dump($_POST);
